fedaeed737421c18bff385cfd49a324a4d0cbfb9
Dennis Nyagah <163292921+Usernyagah@users.noreply.github.com>
2025-06-29 20:37:40 +0300

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: https://taskmanager-5-fs72.onrender.com/,
+  baseURL: 'https://taskmanager-5-fs72.onrender.com/',
 });
 
 export default api;
